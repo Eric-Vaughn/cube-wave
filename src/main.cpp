505a0d4de15322e8b3bdcf7f793c4cd533f00f98
oraclemoss <vaughneric4@gmail.com>
2026-09-20 20:16:30 -0500

    add basic game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
+#include <string>
+#include <raylib.h>
+
+const unsigned int FPS = 60;
+
 int main()
 {
+    InitWindow(800, 800, "Cube Wave"); // Create window
+    SetTargetFPS(FPS);
+
+    while (!WindowShouldClose())
+    {
+
+        // Drawing
+        BeginDrawing();         // Create canvas to draw on
+        ClearBackground(BLACK); // Clear canvas before anything else
+
+        // TODO: Draw objects here
+
+        EndDrawing();
+    }
+    CloseWindow(); // Must close window
 
     return 0;
 }
